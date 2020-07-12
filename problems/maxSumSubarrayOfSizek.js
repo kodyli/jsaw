6ebb4650fad99ca1e82811e89a6a78a7d57cd701
System Development Consultant 9 <SysDevConsultant9@rcoe.us>
2020-07-11 21:32:04 -0700

    Maximun Sum Subarray of Size K (easy)

---
+++ problems/maxSumSubarrayOfSizek.js
@@ -0,0 +1,35 @@
+(function (jsaw) {
+    jsaw.problem({
+        name: 'Maximun Sum Subarray of Size K (easy)',
+        statement: 'Given an array of positive numbers and a positive number "K", find the maximum sum of any contiguous subarray of size "k".',
+        examples: "Input: [2, 1, 5, 1, 3, 2]; Output: 9.",
+        test: function (maxSubarrayOfSizeK) {
+            describe("Maximun Sum Subarray of Size K (easy)", function () {
+                it("Input: [2, 1, 5, 1, 3, 2], k=3; Output: 9.", function () {
+                    expect(maxSubarrayOfSizeK(3, [2, 1, 5, 1, 3, 2])).toEqual(9);
+                });
+                it("Input: [2,3,4,1,5], k=2; Output: 7.", function () {
+                    expect(maxSubarrayOfSizeK(2, [2, 3, 4, 1, 5])).toEqual(7);
+                });
+            });
+        },
+        code: function maxSubarrayOfSizeK(k, numbers) {
+            return 0;
+        },
+        solution: function maxSubarrayOfSizeK(k, numbers) {
+            let maxSum = 0,
+                sum = 0,
+                start = 0;
+            for (let i = 0, l = numbers.length; i < l; i++) {
+                sum += numbers[i];
+                // the size is fixed, so using 'if' statement.
+                if (i >= k - 1) {
+                    maxSum = Math.max(maxSum, sum);
+                    sum -= numbers[start];
+                    start += 1;
+                }
+            }
+            return maxSum;
+        }
+    });
+})(jsaw);
