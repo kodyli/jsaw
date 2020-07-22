94402e9556ca4aa6eec63e0090e1d974057b74d6
System Development Consultant 9 <sysdevconsultant9@rcoe.us>
2020-07-22 00:39:13 -0700

    Level Averages in a Binary Tree (easy)

--- problems/problems.js
+++ problems/problems.js
@@ -670,6 +670,39 @@
 			return [];
 		}
 	});
+	jsaw.problem({
+		name: 'Level Averages in a Binary Tree (easy)',
+		statement: "Given a binary tree, populate an array to represent the averages of all of its levels.",
+		examples: "Input: <img src = 'https://he-s3.s3.amazonaws.com/media/uploads/fdec3c2.jpg'>; Output: [0, 2, 5.5]",
+		test: function (findLevelAverages) {
+			//jasmine style test cases.
+			describe(this.name, function () {
+				it("test", function () {
+					let root = new Node(1);
+					root.left = new Node(2);
+					root.right = new Node(3);
+					root.left.left = new Node(4);
+					root.left.right = new Node(5);
+					root.right.left = new Node(6);
+					root.right.right = new Node(7);
+					expect(findLevelAverages(root)).toEqual([1, 2.5, 5.5]);
+				});
+			});
+		},
+		code: function findLevelAverages(rootNode) {
+			/**
+			 *	function Node(value) {
+			 *		this.value = value;
+			 *		this.left = null;
+			 *		this.right = null;
+			 *	}
+			 */
+			return [];
+		},
+		solution: function findLevelAverages(rootNode) {
+			return [];
+		}
+	});
 	//Dynamic Programming
 	jsaw.problem({
 		name: '0/1 Knapsack',
